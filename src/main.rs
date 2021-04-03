3884af46c5ea0a1d7d5a7643fdcbdea38b255de9
Iron-E <code.iron.e@gmail.com>
2021-04-02 22:47:38 -0400

    ref(main): test `youtube_api`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod youtube_api;
+
+#[tokio::main]
+async fn main()
+{
+	let channel_id = youtube_api::channel_id_of("PragerUniversity").await.unwrap();
+	let videos = youtube_api::random_video_by(&channel_id.unwrap()).await.unwrap();
+	println!("{:?}", videos.unwrap().len());
 }
