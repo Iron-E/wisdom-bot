e0da56d9b4f3ae777b80d216e3ae2f794046dd1f
Iron-E <code.iron.e@gmail.com>
2021-04-10 19:04:05 -0400

    ref(Args): remove custom_url Arg
    It doesn't do what I thought it does.

--- src/discord/wisdom_bot/args.rs
+++ src/discord/wisdom_bot/args.rs
@@ -22,12 +22,6 @@ pub(super) struct Args
 	///
 	/// Optional. The `curstomUrl` of the YouTube user, in case there are multiple users with the
 	/// same `username`.
-	pub custom_url: Option<String>,
-
-	/// # Summary
-	///
-	/// Optional. The `curstomUrl` of the YouTube user, in case there are multiple users with the
-	/// same `username`.
 	pub play_command: String,
 }
 
@@ -41,14 +35,12 @@ impl Args
 		formatcp!(
 "wisdom-bot selects random videos from YouTube channels.
 
-Usage: `'{0} ' Username ('/' CustomUrl)? PlayCommand?`
+Usage: `'{0} ' Username PlayCommand?`
 
 Examples:
 
-- Markiplier video (no `CustomUrl` or `PlayCommand`): `{0} Markiplier`
-- Markiplier video (w/ `CustomUrl`, no `PlayCommand`): `{0} Markiplier/markiplierGAME`
-- Markiplier video (w/ `PlayCommand`, no `CustomUrl`): `{0} Markiplier play`
-- Markiplier video (w/ `CustomUrl` and `PlayCommand`): `{0} Markiplier/markiplierGAME play`",
+- Markiplier video (no `PlayCommand`): `{0} markiplierGAME`
+- Markiplier video (w/ `PlayCommand`): `{0} markiplierGAME play`",
 			WisdomBot::command()
 		)
 	}
@@ -62,16 +54,13 @@ impl FromStr for Args
 	{
 		let mut args = s.split(' ').skip(1);
 
-		let mut username_and_custom_url = match args.next()
-		{
-			Some(u) if !u.is_empty() => u.split('/'),
-			_ => bail!("Must provide a YouTube username. Run `!wisdom` for more information"),
-		};
-
 		Ok(Self
 		{
-			username: username_and_custom_url.next().unwrap().into(),
-			custom_url: username_and_custom_url.next().filter(|url| !url.is_empty()).map(|url| url.into()),
+			username: match args.next()
+			{
+				Some(u) => u.into(),
+				_ => bail!("Must provide a YouTube username. Run `!wisdom` for more information"),
+			},
 			play_command: args.next().map(|cmd| cmd.into()).unwrap_or("play".into()),
 		})
 	}
